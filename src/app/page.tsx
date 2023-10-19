a3bf0662e41f29d63113729faf417bc17b581699
moma <angelmomaa@gmail.com>
2023-10-18 22:52:23 -0600

    Fix social links in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,32 +273,29 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Fullstack Ruby on Rails Security Engineer and Philosopher
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Angel, a developer living in Mexico City.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
+              href="https://twitter.com/angelmoradev"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
             <SocialLink
-              href="https://instagram.com"
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/angel-mora"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://linkedin.com/in/angelmoradev"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
