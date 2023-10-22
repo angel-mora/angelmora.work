a3de9decfbe15132e2567ea851301c9b60e79d91
moma <angelmomaa@gmail.com>
2023-10-21 18:41:02 -0600

    Change my projects copy

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -62,13 +62,13 @@ function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 
 export const metadata: Metadata = {
   title: 'Projects',
-  description: 'Things I’ve made trying to put my dent in the universe.',
+  description: 'Things in the intersection of Philosophy, Technocrique and Code.',
 }
 
 export default function Projects() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
+      title="Things in the intersection of Philosophy, Technocrique and Code."
       intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
     >
       <ul
